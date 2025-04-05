898a0a97df3c100ac13572b9ba73bffc56523152
Abhemanyu Sarin <abhemanyus@gmail.com>
2025-04-05 16:21:59 +0530

    better maneuver

--- src/maneuver.rs
+++ src/maneuver.rs
@@ -25,9 +25,9 @@ pub fn maneuver(
     auto_pilot.set_reference_frame(rf.clone()).mk_call(client)?;
     let ut_node = node.get_ut().mk_call(client)?;
     let deltav = node.get_delta_v().mk_call(client)?;
-    let burn_time = burn_time(client, ship, deltav)?;
-    println!("Burn Time: {burn_time}");
-    let burn_start_time = ut_node - burn_time / 2.0;
+    let (burn_time_before, burn_time_after) = burn_time(client, ship, deltav)?;
+    println!("Burn Time: {}", burn_time_before + burn_time_after);
+    let burn_start_time = ut_node - burn_time_before;
     space_center::warp_to(burn_start_time - 60.0, 100000.0, 2.0).mk_call(client)?;
     auto_pilot
         .set_target_direction(node.burn_vector(rf).mk_call(client)?)
@@ -44,7 +44,7 @@ pub fn maneuver(
         }
     }
     control.set_throttle(1.0).mk_call(client)?;
-    let burn_stop_time = burn_start_time + burn_time;
+    let burn_stop_time = ut_node + burn_time_after;
     loop {
         let update = stream_client.recv_update()?;
         if let Some(val) = update.get_result(&ut_time)? {
@@ -62,14 +62,17 @@ pub fn burn_time(
     client: &mut RPCClient,
     ship: &Vessel,
     deltav: f64,
-) -> Result<f64, Box<dyn Error>> {
+) -> Result<(f64, f64), Box<dyn Error>> {
+    let half_dv = deltav / 2.0;
     let thrust = ship.get_available_thrust().mk_call(client)? as f64;
     let isp = ship.get_specific_impulse().mk_call(client)? as f64 * 9.82;
     let m0 = ship.get_mass().mk_call(client)? as f64;
-    let m1 = m0 / (deltav / isp).exp();
+    let m1 = m0 / (half_dv / isp).exp();
+    let m2 = m1 / (half_dv / isp).exp();
     let flow_rate = thrust / isp;
-    let burn_time = (m0 - m1) / flow_rate;
-    Ok(burn_time)
+    let burn_time_before = (m0 - m1) / flow_rate;
+    let burn_time_after = (m1 - m2) / flow_rate;
+    Ok((burn_time_before, burn_time_after))
 }
 
 pub fn alarm(client: &mut RPCClient, ut_time: f64) -> Result<Event, Box<dyn Error>> {
